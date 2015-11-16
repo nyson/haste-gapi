707761b3605e2cd9966ef950d89fc9d1fcd898d6
Jonathan Skårstedt <jonathan.skarstedt@gmail.com>
2015-11-16 10:33:33 +0100

    Started work on the RequestM type, for concurrent requests.

--- Haste/GAPI/Request.hs
+++ Haste/GAPI/Request.hs
@@ -11,14 +11,15 @@ import Data.Default
 
 import qualified Haste.JSString as JS
 
+-- | Parameters for a GAPI request
 data Params = Params [(String, String)]
             deriving Show
-
 
 instance ToAny Params where
   toAny (Params ps) = let objField (k,v) = (JS.pack k, toAny $ JS.pack v)
                       in toObject $ map objField ps 
 
+-- | Request with parameters and everything
 data Request = Request { path    :: String,
                          method  :: String,
                          params  :: Params,
@@ -34,7 +35,8 @@ instance Default Request where
   def = rawRequest "" []
 
 
--- | Creates a raw JS request
+
+-- | Creates a request by manually entering request path and parameters
 rawRequest :: String -> [(String, String)] -> Request
 rawRequest p kv = Request { path = p,
                             method = "GET",
@@ -42,38 +44,58 @@ rawRequest p kv = Request { path = p,
                             headers = "",
                             body = "" }
 
+
+-- | Applies request and executes the given Promise
+withRequest :: Request -> Promise -> IO ()
+withRequest r p = do re <- jsCreateRequest (path r) (toAny $ params r)
+                     applyPromise re p
+
+-- | Creates a request object 
 jsCreateRequest :: String -> JSAny -> IO JSAny
 jsCreateRequest = ffi "function(p, ps) {\
 \return gapi.client.request({'path': p, 'params': ps})\
 \}"
 
-withRequest :: Request -> Promise -> IO ()
-withRequest r p = do re <- jsCreateRequest (path r) (toAny $ params r)
-                     applyPromise re p
-
+-- | Executes a request and performs the given action as a continuation
 jsExecuteRequestThen :: String -> JSAny -> (Response -> IO ()) -> IO ()
 jsExecuteRequestThen = ffi "function(p, ps, callback) {\
-\ console.debug('invoked jsExecuteRequestThen');\
+\ console.debug('invoked jsExecuteRequestThen');\ 
 \ gapi.client.request({'path': p, 'params': ps}).then({\
 \'then': function(resp) {console.debug('wtf'); callback(resp);}, \
 \'error': function(err) {console.debug('error'); console.debug(err);}\
 \});\
 \}"
 
+
+-- | Executes a request, blocking while waiting for the result and then
+-- return the finished equation
 cRequest :: Request -> CIO JSAny
 cRequest r = do
-  liftIO $ putStrLn $ "creating new mvar..."
   v <- newEmptyMVar
-  liftIO $ putStrLn $ "new mvar created!"
-  
-  let ccb r = concurrent $ do
-        liftIO $ putStrLn $ "putting val in v.."
-        putMVar v r
-        liftIO $ putStrLn $ "v now contains value!"
-
-  liftIO $ putStrLn $ "Executing request..."
-  liftIO $ withRequest r $ Promise ccb $ \_ -> putStrLn "Error"
-  liftIO $ putStrLn $ "Request executed!"
+  liftIO . withRequest r
+    $ Promise (concurrent . putMVar v)
+    $ \_ -> putStrLn "Error"
   takeMVar v
   
-                               
+
+
+data RequestM a = RequestM a
+
+instance Functor RequestM where
+  fmap f (RequestM a) = RequestM $ f a
+
+instance Applicative RequestM where
+  (RequestM f) <*> (RequestM a) = RequestM $ f a
+  pure = RequestM
+
+instance Monad RequestM where
+  (RequestM a) >>= f = f a
+  return = RequestM
+
+instance MonadConc RequestM where
+  liftConc (C a) = RequestM a
+  fork = Haste.Concurrent.fork
+
+
+execute (RequestM a) = a
+
