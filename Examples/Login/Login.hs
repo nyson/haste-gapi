ba155198726ef54d6bb4ba615e28175855766302
Jonathan Skårstedt <jonathan.skarstedt@gmail.com>
2016-04-02 00:14:32 +0200

    updated the login example

--- Examples/Login/Login.hs
+++ Examples/Login/Login.hs
@@ -1,36 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Haste.GAPI
 
-import Haste.DOM
-import Haste.Foreign
+import Haste.GAPI
+import Data.Default
 import qualified Auth
 
-config = Config {
-  -- Your Google API Client ID here 
-  clientID = "",
-  -- The key for your selected API. Not mandatory for all APIs.
-  apiKey = "",
-  -- scopes, in this case google plus
-  scopes = "https://www.googleapis.com/auth/plus.login",
-  -- If true, token is refreshed behind the scenes, not showing any UI.
-  immediate = False}
-
--- Putting stuff to a logging element
-domPut :: String -> IO ()
-domPut str = do
-  entry <- newElem "li" `with` ["innerHTML" =: str]
-  withElem "log" (`appendChild` entry)
-
--- Replace Auth.cfg with your own config
-main = withGAPI Auth.config $ \token -> do
-  if oa2success token
-    then domPut "Successful GAPI Login!"
-    else domPut $ "GAPI Error" ++ errorMsg token
-
-  -- Getting a new token
-  token' <- getToken
-  domPut $ "New token: " ++ show token'
-
-  -- Loading the Google Plus API library with version 'v1'
-  withLibrary (Lib "plus" "v1") $ domPut "Loaded G+ Library!"
-  
+-- | A login example with haste-gapi
+main = withGAPI Auth.config $ \t -> case t of
+  OA2Error {errorMsg = e} -> putStrLn $ "Error lol " ++ e
+  OA2Success {}           -> runR $ do
+    response <- request "plus/v1/people/me" def
+    Just name <- lookupVal response "result.displayName"
+    liftIO $ putStrLn $ "Hello " ++ name ++ "!"
